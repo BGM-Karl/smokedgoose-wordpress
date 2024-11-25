cb5a97b9af0c5a2137a42fcfecbd877e2e38caac
Karl <bgm.karllin@gmail.com>
2024-11-26 01:09:17 +0800

    init

--- wp-includes/class-wpdb.php
+++ wp-includes/class-wpdb.php
@@ -1977,7 +1977,8 @@ class wpdb {
 		if ( $is_ipv6 && extension_loaded( 'mysqlnd' ) ) {
 			$host = "[$host]";
 		}
-
+		$client_flags = MYSQLI_CLIENT_SSL; // 使用 SSL 连接
+		
 		if ( WP_DEBUG ) {
 			mysqli_real_connect( $this->dbh, $host, $this->dbuser, $this->dbpassword, null, $port, $socket, $client_flags );
 		} else {
@@ -2114,7 +2115,8 @@ class wpdb {
 	 * @return bool|void True if the connection is up.
 	 */
 	public function check_connection( $allow_bail = true ) {
-		if ( ! empty( $this->dbh ) && mysqli_ping( $this->dbh ) ) {
+		// Check if the connection is alive.
+		if ( ! empty( $this->dbh ) && mysqli_query( $this->dbh, 'DO 1' ) !== false ) {
 			return true;
 		}
 
@@ -3982,12 +3984,13 @@ class wpdb {
 	 *
 	 * @since 2.5.0
 	 *
-	 * @global string $wp_version             The WordPress version string.
 	 * @global string $required_mysql_version The required MySQL version string.
 	 * @return void|WP_Error
 	 */
 	public function check_database_version() {
-		global $wp_version, $required_mysql_version;
+		global $required_mysql_version;
+		$wp_version = wp_get_wp_version();
+
 		// Make sure the server has the required MySQL version.
 		if ( version_compare( $this->db_version(), $required_mysql_version, '<' ) ) {
 			/* translators: 1: WordPress version number, 2: Minimum required MySQL version number. */
